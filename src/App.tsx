140aa592c9145f7e888c6ec232285c6839057e11
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-03-17 22:13:56 -0300

    header e inicio

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import { Navbar } from "./components/navbar"
 export const App = () => {
 
   return (
-    <div className="w-screen min-h-screen">
+    <div className="w-screen min-h-screen text-white">
       <Navbar/>
+      <div className="flex">
+        <div className="w-full py-3 px-2 text-center flex flex-col justify-center items-center bg-purple-600 dark:bg-purple-700">
+          <span className="text-3xl font-semibold">Conheça aqui os detalhes mais interessantes do jogo.</span>
+          <img src="/paimon.webp" className="py-2"/>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,51 @@
-export const Navbar = () =>{
-    return(
-        <div className="w-full h-12 flex justify-between items-center p-2 bg-purple-500 text-white dark:bg-purple-700">
-            <p>Genshin Wiki</p>
-            <div className="flex gap-2">
-            <button>Sobre</button>     
-            <button>Personagem</button>     
-            <button>Armas</button>     
-            <button>Contato</button>     
-            </div>
-        </div>
-    )
+import { useState } from "react";
+
+interface OptionProps {
+  id: number;
+  title: string;
+  route: string;
 }
+
+export const Navbar = () => {
+  const [option, setOption] = useState<OptionProps[]>([
+    {
+      id: 0,
+      title: "Sobre",
+      route: "/about",
+    },
+    {
+      id: 1,
+      title: "Personagem",
+      route: "/character",
+    },
+    {
+      id: 2,
+      title: "Armas",
+      route: "/weapons",
+    },
+    {
+      id: 3,
+      title: "Contato",
+      route: "/contact",
+    },
+  ]);
+
+  return (
+    <div className="w-full h-12 flex justify-between items-center p-3 bg-purple-500 text-white dark:bg-purple-700">
+      <p>Genshin Wiki</p>
+      <div className="flex gap-2">
+        {option.map(({ id, title, route }) => {
+          return (
+            <a
+              key={id}
+              href={route}
+              className="bg-indigo-700 py-1 px-2 rounded-md transition-colors hover:bg-indigo-500"
+            >
+              {title}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
